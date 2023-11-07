7d3f4c61b63be8e0e6ec7174885d5bcc50418d9b
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2023-11-07 20:59:51 +1100

    login, get routes, jwt

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+// make a server instance 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+	response.json({
+		message:"Hello world"
+	});
+});
+
+const userController = require('./Controllers/UserController');
+app.use("/users", userController);
+
+module.exports = {
+	app
+}
